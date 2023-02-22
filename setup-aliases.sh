e42675c1c87d0d1437c9d683e017c23d7c81a355
Lachlan Underhill <lachlanu@gmail.com>
2023-02-22 11:44:33 +1000

    Tidy up setup_aliases script

--- setup-aliases.sh
+++ setup-aliases.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
 
+aliasFileLoc="$HOME/.aliases"
 declare -A aliases=(
     ["pb"]="ansible-playbook"
-    ["pb-media"]="ansible-playbook media.yml"
-    ["pb-pihole"]="ansible-playbook pihole.yml"
+    ["pbt"]="ansible-playbook main.yml --tags"
 )
 
-aliasFileLoc="$HOME/.aliases"
-
-if [ ! -f $HOME/.aliases ]; then
-    touch $HOME/.aliases
+if [ ! -f $aliasFileLoc ]; then
+    touch $aliasFileLoc
 fi
 
 for alias in "${!aliases[@]}"; do
-    grep -q "^$alias=" "$aliasFileLoc" && sed -i "s/^alias $alias=.*/$alias=${aliases[$alias]}/" "$aliasFileLoc" || echo "alias $alias=${aliases[$alias]}" >> "$aliasFileLoc"
+    grep -q "^alias $alias=" "$aliasFileLoc" && sed -i "s/^alias $alias=.*/alias $alias=\"${aliases[$alias]}\"/" "$aliasFileLoc" || echo "alias $alias=\"${aliases[$alias]}\"" >> "$aliasFileLoc"
 done
 
+# Text formatting
 NORMAL=$(tput sgr0)
 BOLD=$(tput bold)
 
-BLACK=$(tput setaf 0)
-RED=$(tput setaf 1)
-GREEN=$(tput setaf 2)
-YELLOW=$(tput setaf 3)
-LIME_YELLOW=$(tput setaf 190)
-POWDER_BLUE=$(tput setaf 153)
-BLUE=$(tput setaf 4)
 MAGENTA=$(tput setaf 5)
 CYAN=$(tput setaf 6)
-WHITE=$(tput setaf 7)
 
+# Success Message
 printf "Aliases appended to ${MAGENTA}${BRIGHT}$aliasFileLoc${NORMAL}:\n\n"
 for alias in "${!aliases[@]}"; do printf "\t${CYAN}${BOLD}$alias${NORMAL}: ${aliases[$alias]}\n"; done
 printf "\nYou will need to reload your shell to be able to use them (${RED}source ~/.zshrc${NORMAL})\n"
